be85f585ac7cb7e6de693e97c376758009f202f3
Facundo Sanchez <105307321+facusanchez17@users.noreply.github.com>
2024-11-20 12:31:46 -0300

    juego completo, falta estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import confetti from "canvas-confetti";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //array con 9 posiciones vacias
+  const [tablero, setTablero] = useState(Array(9).fill(""));
+
+  //objeto con los dos turnos
+  const turnos = {
+    X: "X",
+    O: "O",
+  };
+
+  //array con posiciones ganadoras
+  const posicionesGanadoras = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  //Array con el turno actual, empieza con la X
+  const [turnoActual, setTurnoActual] = useState(turnos.X);
+
+  //variable para manejar si hay ganador o no
+  const [ganador, setGanador] = useState(false);
+  const [finJuego, setFinJuego] = useState(false);
+
+  //funcion para seteal la celda de posicion
+  function setPosicion(index) {
+    if (ganador || tablero[index]) {
+      return;
+    }
+
+    // Crea una copia del tablero actual
+    const nuevoTablero = [...tablero];
+
+    // Actualiza la posición específica
+    nuevoTablero[index] = turnoActual;
+
+    //seteo el tablero con la nueva posicion
+    setTablero(nuevoTablero);
+
+    // Cambia el turno actual
+    setTurnoActual(turnoActual === turnos.X ? turnos.O : turnos.X);
+
+    //verifico si hay ganador pasandole el nuevo tablero
+    const hayGanador = validarGanador(nuevoTablero);
+    if (hayGanador != false) {
+      setGanador(true);
+      setTurnoActual(hayGanador);
+      confetti();
+    } else {
+      //no hay ganador y el juego termino, verifico en todas las posiciones del tablero
+
+      const hayEmpate = nuevoTablero.every((celda) => celda !== "");
+      if (hayEmpate) {
+        setFinJuego(true);
+      }
+    }
+  }
+
+  function validarGanador(newTablero) {
+    for (const posGanadora of posicionesGanadoras) {
+      const [a, b, c] = posGanadora;
+      if (
+        newTablero[a] &&
+        newTablero[a] === newTablero[b] &&
+        newTablero[a] === newTablero[c]
+      ) {
+        return newTablero[a]; // Retorna el ganador (X u O)
+      }
+    }
+
+    return false; // Si no hay ganador
+  }
+
+  function resetTablero() {
+    setTablero(Array(9).fill(""));
+    setTurnoActual(turnos.X);
+    setGanador(false);
+    setFinJuego(false);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="mt-48">Tablero TATETI</h1>
+      <p>Turno actual: {turnoActual}</p>
+      <button
+        onClick={resetTablero}
+        className="border-2 p-2 hover:bg-slate-400"
+      >
+        Reset
+      </button>
+      <div className="grid grid-cols-3 max-w-64 m-auto mt-4">
+        {tablero.map((valorPosicion, index) => {
+          return (
+            <div
+              key={index}
+              className="p-6 border-2 border-white hover:cursor-pointer hover:bg-slate-800"
+              onClick={() => setPosicion(index)}
+            >
+              {valorPosicion}
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {ganador ? <p className="mt-4">Ganador: {turnoActual}</p> : null}
+      {finJuego ? <p className="mt-4">Fin del juego</p> : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
